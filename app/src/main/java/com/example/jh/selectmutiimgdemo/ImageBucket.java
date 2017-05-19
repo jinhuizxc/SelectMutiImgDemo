54cc0ea4d1ab809c1118d1d131b6f1241100261a
JinHui <hui.jin@boatchina.net>
2017-05-19 15:44:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/selectmutiimgdemo/ImageBucket.java
@@ -0,0 +1,13 @@
+package com.example.jh.selectmutiimgdemo;
+
+
+import com.example.jh.selectmutiimgdemo.model.ImageBean;
+
+import java.util.List;
+
+public class ImageBucket {
+	public int count = 0;
+	public String bucketName;
+	public List<ImageBean> imageList;
+
+}
